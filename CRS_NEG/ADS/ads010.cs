f5287f6705082a6ba8f0e9d27638f9e39e282e1c
FVM <villarroel.fvm@gmail.com>
2021-09-11 05:59:51 +0200

    FV - ABM (TIPO DE IMAGENES)

---
+++ CRS_NEG/ADS/ads010.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CRS_DAT;
+namespace CRS_NEG
+{
+    /// <summary>
+    /// Clase: MODULOS
+    /// </summary>
+    public class ads010
+    {
+        //######################################################################
+        //##       Tabla: ads010                                              ##
+        //##      Nombre: Modulo                                              ##
+        //## Descripcion: Modulos del sistema                                 ##         
+        //##       Autor: FVM - (07-09-2021)                                  ##
+        //######################################################################
+        conexion_a ob_con_ecA = new conexion_a();
+
+
+
+        public string va_ser_bda;//= ob_con_ecA.va_ins_bda;
+
+        public string va_ins_bda;// = ob_con_ecA.va_ins_bda;
+        public string va_nom_bda;//= ob_con_ecA.va_nom_bda;
+        public string va_ide_usr;//= ob_con_ecA.va_ide_usr;
+        public string va_pas_usr;//= ob_con_ecA.va_pas_usr;
+
+
+        DataTable Tabla = new DataTable();
+        string cadena;
+        public ads010()
+        {
+
+            va_ser_bda = ob_con_ecA.va_ser_bda;
+            va_ins_bda  = ob_con_ecA.va_ins_bda;
+            va_nom_bda = ob_con_ecA.va_nom_bda;
+            va_ide_usr = ob_con_ecA.va_ide_usr;
+            va_pas_usr = ob_con_ecA.va_pas_usr;
+        }
+
+
+
+        public DataTable Fe_obt_mod()
+        {
+            cadena = "SELECT * FROM ads010 " +
+                " WHERE va_est_ado = 'H' ";
+            Tabla =  ob_con_ecA.fe_exe_sql(cadena);
+
+            return Tabla;
+        }
+
+        public void Fe_crea(int ar_ide_tip , string ar_nom_tip , string ar_ide_tab , string ar_est_ado)
+        {
+            cadena = " INSERT INTO ads010 VALUES(" + ar_ide_tip + ",'" + ar_nom_tip + "' , '" + ar_ide_tab + "', 'H')";
+
+
+            ob_con_ecA.fe_exe_sql(cadena);
+        }
+
+
+        public void Fe_edi_mod (int ar_ide_tip , string ar_nom_tip , string ar_ide_tab )
+        {
+            cadena = " UPDATE ads010 SET va_nom_tip = '" + ar_nom_tip + "' , va_ide_tab = '" + ar_ide_tab + "'" +
+                    " WHERE va_ide_tip = " + ar_ide_tip ;
+            ob_con_ecA.fe_exe_sql(cadena);
+        }
+
+        public void Fe_hab_ili(int ar_ide_tip )
+        {
+            cadena = " UPDATE ads010 SET va_est_ado = 'H'" +
+                    " WHERE va_ide_tip = " + ar_ide_tip ;
+            ob_con_ecA.fe_exe_sql(cadena);
+        }
+        public void Fe_des_hab(int ar_ide_tip )
+        {
+            cadena = " ads010_04a_p01 '" + ar_ide_tip + "'";
+            ob_con_ecA.fe_exe_sql(cadena);
+        }
+
+
+        public void Fe_eli_mod (int ar_ide_tip )
+        {
+            cadena = " ads010_06a_p01 " + ar_ide_tip + "";
+            ob_con_ecA.fe_exe_sql(cadena);
+        }
+
+        public DataTable Fe_con_mod (string ar_ide_tip )
+        {
+            cadena = " SELECT * FROM ads010 WHERE va_ide_tip =  " + ar_ide_tip + " ";
+            return ob_con_ecA.fe_exe_sql(cadena);
+        }
+
+
+        public DataTable Fe_bus_car(string ar_tex_bus, int ar_par_ame, string ar_est_ado)
+        {
+            cadena = " SELECT * FROM ads010 ";
+            if (ar_par_ame == 0)
+                cadena += " WHERE va_ide_tip like '" + ar_tex_bus + "%'";
+            if (ar_par_ame == 1)
+                cadena += " WHERE va_nom_tip like '" + ar_tex_bus + "%'";
+            if (ar_par_ame == 2)
+                cadena += " WHERE va_ide_tab like '" + ar_tex_bus + "%'";
+            
+
+
+            if (ar_est_ado != "T")
+                cadena += " AND va_est_ado ='" + ar_est_ado + "'";
+
+
+            return ob_con_ecA.fe_exe_sql(cadena);
+        }
+
+
+        //** FUNCIONES DE REPORTES
+
+        /// <summary>
+        /// Funcion externa reporte: TIPOS DE USUARIOS
+        /// </summary>
+        /// <param name="ar_ide_tip "> Ide Modulo</param>
+        /// <param name="ar_est_ado"> Estado</param>
+        /// <returns></returns>
+        public DataTable Fe_ads010_R01(int ar_ide_tip , string ar_est_ado)
+        {
+            cadena = " ads010_R01 " + ar_ide_tip + ", '" + ar_est_ado + "'";
+
+            return ob_con_ecA.fe_exe_sql(cadena);
+        }
+
+
+    }
+}
---
+++ CRS_PRE/ADS/ads010_02.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+using CRS_NEG;
+
+namespace CRS_PRE
+{
+    public partial class ads010_02 : Form
+    {
+
+        public dynamic frm_pad;
+        public int frm_tip;
+        //Instancias
+        ads010 o_ads010 = new ads010();
+
+        DataTable tabla = new DataTable();
+
+
+        public ads010_02()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void frm_Load(object sender, EventArgs e)
+        {
+           
+            tb_ide_tip.Focus();
+        }
+
+        protected string Fi_val_dat()
+        {
+
+            if (tb_ide_tip.Text.Trim() == "")
+            {
+                tb_ide_tip.Focus();
+                return "Debe proporcionar el Codigo";
+            }
+
+            if (cl_glo_bal.IsNumeric(tb_ide_tip.Text) == false)
+            {
+                tb_ide_tip.Focus();
+                return "El codigo no es valido";
+            }
+
+            //Verificar 
+            tabla = o_ads010.Fe_con_mod(tb_ide_tip.Text);
+            if (tabla.Rows.Count > 0)
+            {
+                tb_ide_tip.Focus();
+                return "El Tipo de Imagen que desea crear ya se encuentra registrado";
+            }
+            if (tb_nom_tip.Text.Trim() == "")
+            {
+                tb_nom_tip.Focus();
+                return "Debe proporcionar el Nombre";
+            }
+
+            if (tb_ide_tab.Text.Trim() == "")
+            {
+                tb_nom_tip.Focus();
+                return "Debe proporcionar la Tabla";
+            }
+
+
+            return "";
+        }
+
+        private void Fi_lim_pia()
+        {
+           
+            tb_ide_tip.Clear(); 
+            tb_nom_tip.Clear();
+            tb_ide_tab.Clear();
+           
+            tb_ide_tip.Focus();
+        }
+        private void Bt_can_cel_Click(object sender, EventArgs e)
+        {
+            cl_glo_frm.Cerrar(this);
+        }
+
+        private void Bt_ace_pta_Click(object sender, EventArgs e)
+        {
+            string msg_val = "";
+            DialogResult msg_res;
+
+            // funcion para validar datos
+            msg_val = Fi_val_dat();
+            if (msg_val != "")
+            {
+                MessageBox.Show(msg_val, "Error", MessageBoxButtons.OK);
+                return;
+            }
+            msg_res = MessageBox.Show("Esta seguro de registrar la informacion?", "Nuevo Tipo de Imagen", MessageBoxButtons.OKCancel);
+                if (msg_res == DialogResult.OK)
+            {
+                //Registrar 
+                o_ads010.Fe_crea(int.Parse(tb_ide_tip.Text), tb_nom_tip.Text, tb_ide_tab.Text,"H");
+                MessageBox.Show("Los datos se grabaron correctamente", "Nuevo Tipo de Imagen", MessageBoxButtons.OK);
+                Fi_lim_pia();
+            }
+
+        }
+
+        private void tb_ide_mod_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            cl_glo_bal.NotNumeric(e);          
+        }
+    }
+}
